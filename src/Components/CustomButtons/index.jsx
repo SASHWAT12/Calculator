2d87c5af4d8fd37c37b2765c3a889ab693cfbee0
SASHWAT12 <65287899+SASHWAT12@users.noreply.github.com>
2024-12-22 22:54:26 +0530

    Added Button Component

---
+++ src/Components/CustomButtons/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CustomButtons = ({label})=>{
+    return(
+        <div>
+            <h1>{label}</h1>
+        </div>
+    );
+};
+
+export default CustomButtons;
--- src/Components/Keypad/index.jsx
+++ src/Components/Keypad/index.jsx
@@ -1,10 +1,24 @@
 import React from "react";
 import "./index.css";
+import CustomButtons from "../CustomButtons";
 
 const Keypad = () => {
+    const buttons = ['7','8','9','/',
+                     '4','5','6','*',
+                     '1','2','3','-',
+                     '0',',','+','=',
+                     'AC'];
+
     return (
         <div className="keypadBox">
-
+            {buttons.map((btn)=>
+                (
+                    <CustomButtons
+                    key={btn} 
+                    label={btn} 
+                    />
+                )
+            )};
         </div>
     );
 }
